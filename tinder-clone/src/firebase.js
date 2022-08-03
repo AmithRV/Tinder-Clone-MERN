82b5d6eb31fc4440b6a17b1e9a713003245ae710
AmithRV <amithrajv1999@gmail.com>
2022-08-03 18:53:56 +0530

    commit

---
+++ tinder-clone/src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBEWuk_m93vDHtMOwg-nzB6rFUvllUuoAM",
+  authDomain: "tinder-clone-frontend-d3ea0.firebaseapp.com",
+  projectId: "tinder-clone-frontend-d3ea0",
+  storageBucket: "tinder-clone-frontend-d3ea0.appspot.com",
+  messagingSenderId: "761774919640",
+  appId: "1:761774919640:web:dcce8dad61cbaf76952cad"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+//https://tinder-clone-frontend-d3ea0.web.app
+//https://tinder-clone-frontend-d3ea0.web.app
+//https://tinder-clone-backend-nodejs-11.herokuapp.com/tinder/create-card
